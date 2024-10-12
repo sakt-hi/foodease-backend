f8e98e1ddcbff8978c7d97df7b2f18638e412456
sakt-hi <sakthivel.ganesan@hotmail.com>
2024-10-12 11:53:51 +0530

    order-sorting

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -75,7 +75,7 @@ const verifyOrder = async (req, res) => {
 //user orders for frontend
 const userOrders = async (req, res) => {
     try {
-        const orders = await orderModel.find({ userId: req.body.userId });
+        const orders = await orderModel.find({ userId: req.body.userId }).sort({ date: -1 });;
         res.json({ success: true, data: orders });
     } catch (error) {
         console.log(error);
